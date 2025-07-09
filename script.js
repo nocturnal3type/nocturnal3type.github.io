2c3d926e9c00757786854ad141d7087179b6ce56
nocturnal3type <nocturnal3type@gmail.com>
2025-07-10 02:16:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -48,15 +48,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   function resetInfoPanel() {
-    infoPanel.innerHTML = `
-      <h2>초밥이</h2>
-      <p>안녕하세요? 왜 안녕하신가요? 부럽습니다.</p>
-      <p>이쪽은 제 여자친구들입니다. 캐릭터 누르면 설명 뜹니다. 아니? 캐릭터 아닙니다. 엄연한 제 가족들입니다.</p>
-    `;
+  const panel = document.getElementById("info-panel");
+  const title = panel.dataset.title;
+  const line1 = panel.dataset.line1;
+  const line2 = panel.dataset.line2;
 
-    const windowTitle = document.querySelector(".window .title-bar");
-    windowTitle.style.backgroundColor = "navy";
-  }
+  panel.innerHTML = `
+    <h2>${title}</h2>
+    <p>${line1}</p>
+    <p>${line2}</p>
+  `;
+
+  const windowTitle = document.querySelector(".window .title-bar");
+  windowTitle.style.backgroundColor = "navy";
+ }
 });
 
   
